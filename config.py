ca25152dd2cdb1bc2920b28567b70ed2e11ae0e6
cancinconnnbot <81495310+cancinconnnbot@users.noreply.github.com>
2021-06-17 18:41:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
         LOG_GROUP=int(LOG_GROUP)
     else:
         LOG_GROUP=None
-    STREAM_URL=os.environ.get("STREAM_URL", "https://radioindia.net/radio/hungamanow/icecast.audio")
+    STREAM_URL=os.environ.get("STREAM_URL", "http://46.20.3.204/listen.pls")
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     HELL_SESSION = os.environ.get("HELL_SESSION", "")
