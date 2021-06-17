4ed9953dbe6351fe81c56866dd7ee5308339bc91
cancinconnnbot <81495310+cancinconnnbot@users.noreply.github.com>
2021-06-17 19:00:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
         LOG_GROUP=int(LOG_GROUP)
     else:
         LOG_GROUP=None
-    STREAM_URL=os.environ.get("STREAM_URL", "https://www.kralmuzik.com.tr/radyo/kral-fm#")
+    STREAM_URL=os.environ.get("STREAM_URL", "https://www.radio.net/s/kralfm")
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     HELL_SESSION = os.environ.get("HELL_SESSION", "")
